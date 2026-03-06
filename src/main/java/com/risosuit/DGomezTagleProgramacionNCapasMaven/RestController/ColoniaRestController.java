94aba5662d4d29d1f814fc66e5566dbd2faa66be
Diego GTG <diegogomeztg@outlook.com>
2026-03-06 16:24:18 -0600

    Colonia RESTcontroller

---
+++ src/main/java/com/risosuit/DGomezTagleProgramacionNCapasMaven/RestController/ColoniaRestController.java
@@ -0,0 +1,44 @@
+package com.risosuit.DGomezTagleProgramacionNCapasMaven.RestController;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.DAO.ColoniaJPADAOImplementation;
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.DAO.MunicipioJPADAOImplementation;
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.JPA.Result;
+
+@RestController
+@RequestMapping("api/colonia")
+public class ColoniaRestController {
+    @Autowired
+    private ColoniaJPADAOImplementation coloniaJPADAOImplementation;
+
+    @GetMapping("{idMunicipio}")
+    public ResponseEntity GetMunicipioByEstado(@PathVariable("idMunicipio") int idMunicipio) {
+        try {
+            Result Result = coloniaJPADAOImplementation.getColoniaByMunicipio(idMunicipio);
+            if (Result.Correct) {
+                if (Result.Objects != null || !Result.Objects.isEmpty()) {
+                    return ResponseEntity.ok(Result);
+                } else {
+                    return ResponseEntity.noContent().build();
+                }
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", e.getMessage()));
+        }
+
+    }
+
+}
