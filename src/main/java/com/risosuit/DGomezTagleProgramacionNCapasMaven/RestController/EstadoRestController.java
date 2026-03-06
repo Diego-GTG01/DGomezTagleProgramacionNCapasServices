995b757ed8ee6e3ea3bf77c1546c5e333cec2459
Diego GTG <diegogomeztg@outlook.com>
2026-03-06 16:27:39 -0600

    Estados RestController

---
+++ src/main/java/com/risosuit/DGomezTagleProgramacionNCapasMaven/RestController/EstadoRestController.java
@@ -0,0 +1,44 @@
+package com.risosuit.DGomezTagleProgramacionNCapasMaven.RestController;
+
+import java.util.Map;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.DAO.EstadoJPADAOImplementation;
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.JPA.Result;
+
+@RestController
+@RequestMapping("api/estado")
+public class EstadoRestController {
+    @Autowired
+    private EstadoJPADAOImplementation estadoJPADAOImplementation;
+
+    @GetMapping("{idPais}")
+    public ResponseEntity GetEstadoByPais(@PathVariable("idPais") int idPais) {
+        try {
+            Result Result = estadoJPADAOImplementation.GetgetEstadoByPais(idPais);
+            if (Result.Correct) {
+                if (Result.Objects != null || !Result.Objects.isEmpty()) {
+                    return ResponseEntity.ok(Result);
+                } else {
+                    return ResponseEntity.noContent().build();
+                }
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", e.getMessage()));
+        }
+
+    }
+
+}
