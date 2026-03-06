d9d6f6b6e255d33c963e55f9dedf17acb2286f6d
Diego GTG <diegogomeztg@outlook.com>
2026-03-06 16:28:11 -0600

    Municipio Rest Controller

---
+++ src/main/java/com/risosuit/DGomezTagleProgramacionNCapasMaven/RestController/MunicipioRestController.java
@@ -0,0 +1,43 @@
+package com.risosuit.DGomezTagleProgramacionNCapasMaven.RestController;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.DAO.MunicipioJPADAOImplementation;
+import com.risosuit.DGomezTagleProgramacionNCapasMaven.JPA.Result;
+
+@RestController
+@RequestMapping("api/municipio")
+public class MunicipioRestController {
+
+    @Autowired
+    private MunicipioJPADAOImplementation municipioJPADAOImplementation;
+
+    @GetMapping("{idEstado}")
+    public ResponseEntity GetMunicipioByEstado(@PathVariable("idEstado") int idEstado) {
+        try {
+            Result Result = municipioJPADAOImplementation.getMunicipioByEstado(idEstado);
+            if (Result.Correct) {
+                if (Result.Objects != null || !Result.Objects.isEmpty()) {
+                    return ResponseEntity.ok(Result);
+                } else {
+                    return ResponseEntity.noContent().build();
+                }
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", e.getMessage()));
+        }
+
+    }
+
+}
